94b00bcbb25d893ea3bd1ef5b089e4144234bd4c
denis-cohen <denis.cohen.132@gmail.com>
2026-04-22 10:13:40 +0200

    Minor fix

--- rmd/render.r
+++ rmd/render.r
@@ -28,7 +28,7 @@ inline_child <- function(lines, output_path) {
 
 ## Meta information ----
 meta <- rio::import("rmd/meta.csv") %>%
-  dplyr::slice(10)
+  dplyr::slice(11)
 formats <- c("learnr")
 
 ## Apply function within nested loop ----
